5b4ba34ef4d118f63abfe5816f136fd0c6bc3407
Arnaud Gagnieu <arnaud.gagnieu@hotmail.fr>
2021-02-08 16:03:22 +0100

    code before review Ranga

--- Controller/menu.py
+++ Controller/menu.py
@@ -1,43 +1,39 @@
 """Controller to display menu and inputs"""
 
 
-def tournament_input():
-    questions = ["Tournament's name:   ",
-                 "Tournament's adress:   ",
-                 "Tournament's Date:   ",
-                 "Total number of rounds:   ",
-                 "Time control:   ",
-                 "Number of player:   ",
-                 "comments:   "
-                 ] 
+class TournamentInput:
+                     
+    def t_input(self):
 
-    tour_infos = []
-    
-    for i in questions:
-        if i is questions[3]:
-            try:
-                tour_infos.append(int(input(i)))
+        tour_infos = []
 
-            except ValueError:
-                print('Please enter an integer')
-                tour_infos.append(int(input(i)))
+        questions = ["Tournament's name:   ",
+                     "Tournament's adress:   ",
+                     "Tournament's Date:   ",
+                     "Total number of rounds:   ",
+                     "Time control:   ",
+                     "Number of player:   ",
+                     "comments:   "
+                    ] 
 
-        elif i is questions[5]:
-            try:
-                tour_infos.append(int(input(i)))
+        for i in questions:
+            if i is questions[3]:
+                try:
+                    tour_infos.append(int(input(i)))
 
-            except ValueError:
-                print('Please enter an integer')
-                tour_infos.append(int(input(i)))
+                except ValueError:
+                    print('Please enter an integer')
+                    tour_infos.append(int(input(i)))
 
-        else:
-            tour_infos.append(input(i))
+            elif i is questions[5]:
+                try:
+                    tour_infos.append(int(input(i)))
 
-    return tour_infos
+                except ValueError:
+                    print('Please enter an integer')
+                    tour_infos.append(int(input(i)))
 
-tournament_infos = tournament_input()
+            else:
+                tour_infos.append(input(i))
 
-print(tournament_infos)
-
-
-tournament_infos
+        return tour_infos
